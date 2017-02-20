0b9e13c2940d315388b89b26ee36e9ae96c0de4e
Jason Hansen <jason_hansen@outlook.com>
2017-02-20 00:47:21 -0700

    improve hook

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ lore.summon({
     dialog: require('lore-hook-dialog'),
     dialogs: require('lore-hook-dialogs-bootstrap'),
     models: require('lore-hook-models'),
-    polling: require('./lore-hook-polling'),
+    polling: require('./polling-hook'),
     reducers: require('lore-hook-reducers'),
     redux: _.extend(require('lore-hook-redux'), {
       dependencies: ['reducers', 'auth']
--- polling-hook/src/index.js
+++ polling-hook/src/index.js
@@ -1,13 +1,52 @@
 var _ = require('lodash');
 
-function poll(action, config, query={}) {
+/**
+ * Flatten javascript objects into a single-depth object
+ * https://gist.github.com/penguinboy/762197
+ */
+function flattenObject(ob) {
+  var toReturn = {};
+
+  for (var i in ob) {
+    if (!ob.hasOwnProperty(i)) continue;
+
+    if ((typeof ob[i]) == 'object') {
+      var flatObject = flattenObject(ob[i]);
+      for (var x in flatObject) {
+        if (!flatObject.hasOwnProperty(x)) continue;
+
+        toReturn[i + '.' + x] = flatObject[x];
+      }
+    } else {
+      toReturn[i] = ob[i];
+    }
+  }
+  return toReturn;
+}
+
+/**
+ * Call the action (with the bound arguments) every [interval] milliseconds
+ */
+function poll(action, config) {
   // invoke the action
-  action(query);
+  action();
 
   // wait the specified interval, then invoke the action again
   setTimeout(function() {
-    poll(action, config, query);
+    poll(action, config);
   }, config.interval);
+}
+
+function createPollingWrapper(action, config) {
+  return function callAction() {
+    // Create a version of the action that is bound to the arguments provided by the
+    // user. This makes sure the hook will work with any arbitrary function - it simply
+    // invokes that action with the provided arguments on the requested interval
+    var boundAction = Function.prototype.apply.bind(action).bind(null, null, arguments);
+
+    // Begin polling the action
+    return poll(boundAction, config);
+  }
 }
 
 module.exports = {
@@ -21,24 +60,31 @@ module.exports = {
   },
 
   load: function(lore) {
-    // get the actions so we can make them pollable
+    // 1. Get the actions so we can make them pollable
     var actions = lore.actions;
 
-    // get the application level config (defaults + config/polling.js)
+    // 2. Get the application level config (defaults + config/polling.js)
     var appConfig = lore.config.polling;
 
-    // get the model specific config (for tweet)
-    var modelConfig = lore.loader.loadModels().tweet;
+    // 3. Get the model specific configs
+    var modelConfigs = lore.loader.loadModels();
 
-    // combine values from both configs, giving priority to values in the model config
-    var config = _.defaults({}, modelConfig.polling, appConfig);
+    // 4. Create a polling object that will mirror the structure of the actions object
+    lore.polling = {};
 
-    // expose our target tweet.find action in a poll-able container
-    lore.polling = {
-      tweet: {
-        find: poll.bind(null, actions.tweet.find, config)
-      }
-    };
+    // 5. Iterate over each action and create a pollable version attached to the polling object
+    _.mapKeys(flattenObject(actions), function (action, actionKey) {
+      // 6. Get the model specific config
+      var modelName = actionKey.split('.')[0];
+      var modelConfig = modelConfigs[modelName];
+
+      // 7. Combine values from both configs, giving priority to values in the model config
+      var config = _.defaults({}, modelConfig.polling, appConfig);
+
+      // 8. Generate the pollable version of the action
+      _.set(lore.polling, actionKey, createPollingWrapper(action, config));
+    });
   }
 
 };
+
