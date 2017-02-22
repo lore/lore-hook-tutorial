ce6eaaf32eefa6d7d908e4fa4925b5e0e7794309
Jason Hansen <jason_hansen@outlook.com>
2017-02-21 21:15:26 -0700

    convert hook to ES6 syntax

--- polling-hook/src/index.js
+++ polling-hook/src/index.js
@@ -1,18 +1,18 @@
-var _ = require('lodash');
+import _ from 'lodash';
 
 /**
  * Flatten javascript objects into a single-depth object
  * https://gist.github.com/penguinboy/762197
  */
 function flattenObject(ob) {
-  var toReturn = {};
+  const toReturn = {};
 
   for (var i in ob) {
     if (!ob.hasOwnProperty(i)) continue;
 
     if ((typeof ob[i]) == 'object') {
-      var flatObject = flattenObject(ob[i]);
-      for (var x in flatObject) {
+      const flatObject = flattenObject(ob[i]);
+      for (let x in flatObject) {
         if (!flatObject.hasOwnProperty(x)) continue;
 
         toReturn[i + '.' + x] = flatObject[x];
@@ -42,7 +42,7 @@ function createPollingWrapper(action, config) {
     // Create a version of the action that is bound to the arguments provided by the
     // user. This makes sure the hook will work with any arbitrary function - it simply
     // invokes that action with the provided arguments on the requested interval
-    var boundAction = Function.prototype.apply.bind(action).bind(null, null, arguments);
+    const boundAction = Function.prototype.apply.bind(action).bind(null, null, arguments);
 
     // Begin polling the action
     return poll(boundAction, config);
@@ -61,13 +61,13 @@ module.exports = {
 
   load: function(lore) {
     // 1. Get the actions so we can make them pollable
-    var actions = lore.actions;
+    const actions = lore.actions;
 
     // 2. Get the application level config (defaults + config/polling.js)
-    var appConfig = lore.config.polling;
+    const appConfig = lore.config.polling;
 
     // 3. Get the model specific configs
-    var modelConfigs = lore.loader.loadModels();
+    const modelConfigs = lore.loader.loadModels();
 
     // 4. Create a polling object that will mirror the structure of the actions object
     lore.polling = {};
@@ -75,11 +75,11 @@ module.exports = {
     // 5. Iterate over each action and create a pollable version attached to the polling object
     _.mapKeys(flattenObject(actions), function (action, actionKey) {
       // 6. Get the model specific config
-      var modelName = actionKey.split('.')[0];
-      var modelConfig = modelConfigs[modelName];
+      const modelName = actionKey.split('.')[0];
+      const modelConfig = modelConfigs[modelName];
 
       // 7. Combine values from both configs, giving priority to values in the model config
-      var config = _.defaults({}, modelConfig.polling, appConfig);
+      const config = _.defaults({}, modelConfig.polling, appConfig);
 
       // 8. Generate the pollable version of the action
       _.set(lore.polling, actionKey, createPollingWrapper(action, config));
